bf03aad0b2e9f018927deddb6d8b9875d9ec235d
Mitch T <mtexel13@unl.edu>
2023-02-08 15:30:30 -0600

    Added a debug log

--- Rollaball/Assets/Scripts/TiltScript.cs
+++ Rollaball/Assets/Scripts/TiltScript.cs
@@ -6,20 +6,18 @@ using Valve.VR.InteractionSystem;
 
 public class TiltScript : MonoBehaviour
 {
-    public Transform Joystick;
-    public float joyMove = 0.1f;
+    //public Transform Joystick;
+    //public float joyMove = 0.1f;
 
     public SteamVR_Action_Vector2 moveAction = SteamVR_Input.GetAction<SteamVR_Action_Vector2>("platformer", "Move");
     public SteamVR_Action_Boolean jumpAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("platformer", "Jump");
 
     //public JoeJeff character;
 
-    public Renderer jumpHighlight;
-
     private Vector3 movement;
     private bool jump;
     private float glow;
-    private SteamVR_Input_Sources hand;
+    public SteamVR_Input_Sources hand;
     private Interactable interactable;
 
     private void Start()
@@ -31,7 +29,8 @@ public class TiltScript : MonoBehaviour
     {
 
         Vector2 m = moveAction[hand].axis;
-        transform.Rotate(new Vector3(m.x, 0, m.y));
+        Debug.Log("X: " + m.x);
+        transform.Rotate(new Vector3(m.x*10, 0, m.y*10));
         //transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime);
     }
 }
